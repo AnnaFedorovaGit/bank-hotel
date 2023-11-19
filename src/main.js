5b2e158832c6ceb8246317b9c122f62e47e283ea
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2023-11-19 15:30:23 +0200

    Add BEM and SCSS

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './css/style.scss';
+
+import './js/mobile-menu.js';
+// import './js/header.js';
+// import './js/authorisation.js';
+// import './js/category.js';
+// import './js/charities.js';
+// import './js/books.js';
+// import './js/popup.js';
+// import './js/footer.js';
+// import './js/scroll-up.js';
